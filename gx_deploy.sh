78e3a71ea4a70aaca6b29bb80bb29fa027c5feef
Slim <755808379@qq.com>
2021-03-24 10:25:13 +0800

    Update gx_deploy.sh

--- gx_deploy.sh
+++ gx_deploy.sh
@@ -2,12 +2,12 @@
 
 FEATURE=$1
 PORT=$2
-cp -r ~/apps/aizuoye-server ~/apps/$PORT
+cp -r /home/deploy/apps/aizuoye-server /home/deploy/apps/$PORT
 cp /etc/nginx/conf.d/homework.conf /etc/nginx/conf.d/$PORT.conf
 sed -i "s/80/$PORT/g" /etc/nginx/conf.d/$PORT.conf
 sed -i "s/aizuoye-server/$PORT/g" /etc/nginx/conf.d/$PORT.conf
 nginx -s reload
-cd ~/apps/$PORT/server/php
+cd /home/deploy/apps/$PORT/server/php
 git checkout master
 git pull
 git checkout $FEATURE
